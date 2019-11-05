792cc8421d65eee5ae740be6b29e99ee7a8621fb
luan.sabino <39681078+luan-sabino@users.noreply.github.com>
2019-11-05 09:31:40 -0300

    render function + additens function
    render function fully developed and operational and has been created a function with the function of add more itens to de first column of to-do's

--- funcoes.js
+++ funcoes.js
@@ -1,26 +1,57 @@
 $(function(){
     
-    let tarefas = 0;
+    var tarefas = []
 
+    var toDoListElement = document.querySelector("#lista-interface #to-do #to-do-body");
+    
     $("#botao-f").click(function(){
         $("#popup").css("display","flex");
         $("#botao-f").css("display","none");
     });
 
-    $(document).on("keydown",function(e){
-        if((e.which = 13) && e.altKey){
-            let tituloTarefaNova = $("#titulo-t").val();
-            let conteudoTarefaNova = $("#conteudo-t").val();
-            let div = d3
-            .select("#to-do")
-            .data([])
+    function render(){
+        toDoListElement.innerHTML = '';
 
-            .select("#to-do")
+        for(tarefa of tarefas){
+            var todoElement = document.createElement('div')
+            var todoElementTextTitle = document.createElement('textarea')
+            var todoElementTextContent = document.createElement('textarea')
+
+            var textTitle = document.createTextNode(tarefa.titulo);
+            var textContent = document.createTextNode(tarefa.conteudo);
+
+            todoElementTextTitle.appendChild(textTitle);
+            todoElementTextContent.appendChild(textContent);
+
+            todoElement.setAttribute('class', 'tarefas');
+            todoElementTextTitle.setAttribute('class', 'titulo-ef');
+            todoElementTextContent.setAttribute('class', 'conteudo-ef');
+            todoElementTextTitle.setAttribute('readonly', 'readonly');
+            todoElementTextContent.setAttribute('readonly', 'readonly');
+
+            todoElement.appendChild(todoElementTextTitle);
+            todoElement.appendChild(todoElementTextContent);
+            toDoListElement.appendChild(todoElement);
             
+        }
+    }
+
+    $(document).on("keydown",function(e){
+        if((e.which == 13) && e.altKey){
+            var tituloT = $("#titulo-t").val();
+            var conteudoT = $("#conteudo-t").val();
+
+            tarefas.push({'titulo': tituloT,'conteudo':conteudoT});
+
+            render();
+
+            $("#titulo-t").val('');
+            $("#conteudo-t").val('');
 
             $("#popup").css("display","none");
             $("#botao-f").css("display","block");
         }
+        
     })
 
 });
