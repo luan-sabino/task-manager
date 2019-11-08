3bebb726be47d610821526836f06fdbe1fd0b3d9
luan.sabino <39681078+luan-sabino@users.noreply.github.com>
2019-11-08 00:46:02 -0300

    Revert "Update"

--- funcoes.js
+++ funcoes.js
@@ -3,8 +3,6 @@ $(function(){
     var tarefas = []
 
     var toDoListElement = document.querySelector("#lista-interface #to-do #to-do-body");
-    var transferElement = document.querySelectorAll("#lista-interface #to-do #to-do-body");
-
     
     $("#botao-f").click(function(){
         $("#popup").css("display","flex");
@@ -22,10 +20,6 @@ $(function(){
             var textTitle = document.createTextNode(tarefa.titulo);
             var textContent = document.createTextNode(tarefa.conteudo);
 
-            var pos = tarefas.indexOf(tarefa);
-
-            todoElementTextTitle.setAttribute('id', pos);
-
             todoElementTextTitle.appendChild(textTitle);
             todoElementTextContent.appendChild(textContent);
 
@@ -42,49 +36,22 @@ $(function(){
         }
     }
 
-    function endPopUp(){
-        $("#titulo-t").val('');
-        $("#conteudo-t").val('');
-
-        $("#popup").css("display","none");
-        $("#botao-f").css("display","block");
-    }
-
     $(document).on("keydown",function(e){
-        
         if((e.which == 13) && e.altKey){
             var tituloT = $("#titulo-t").val();
             var conteudoT = $("#conteudo-t").val();
 
-            if(tituloT === '' || conteudoT === ''){
-                endPopUp();
-                
-            }else{
-                tarefas.push({'titulo': tituloT,'conteudo':conteudoT});
+            tarefas.push({'titulo': tituloT,'conteudo':conteudoT});
 
-                render();
-    
-                endPopUp();
-            }
+            render();
 
+            $("#titulo-t").val('');
+            $("#conteudo-t").val('');
 
-        }
-
-        if(e.which == 27){
-            endPopUp();
+            $("#popup").css("display","none");
+            $("#botao-f").css("display","block");
         }
         
     })
-
-    // function progress(){
-    //     var divsToDo = $("#to-do-body");
-    //     var tam = divsToDo.length;
-    //     console.log(tam);
-    //     alert(tam);
-    // }
-
-    // setInterval(progress(), 1000);
-
-    
 
 });
